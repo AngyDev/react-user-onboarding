a74c1636ca17fb7b0b9700c60c413fe0a02f99e4
Angela Busato <busato.angela@gmail.com>
2021-04-30 18:08:25 +0200

    [add] font style to the app component

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Logo from "./components/Logo";
 
 export default class App extends React.Component {
     render() {
         return(
             <div>
-                <h1>Hello World</h1>
+                <Logo />                
+                <h1 style={{fontFamily: "Montserrat Alternates"}}>Hello World</h1>
             </div>
         )
     }
