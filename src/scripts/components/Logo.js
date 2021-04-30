36ceb91056de05cf5ceba9f0287fb9a28bb9bc42
Angela Busato <busato.angela@gmail.com>
2021-04-30 18:08:41 +0200

    [add] Logo app component

---
+++ src/scripts/components/Logo.js
@@ -0,0 +1,20 @@
+/* import React from "react";
+import logo from "../../images/logo.svg";
+
+export default function Logo() {
+    return(
+        <div>
+            <img src={logo} alt="Logo"/>
+        </div>
+    )
+} */
+
+import React from "react";
+
+export default function Logo() {
+    return(
+        <div>
+            <img src="./images/logo.svg" alt="Logo"/>
+        </div>
+    )
+}
