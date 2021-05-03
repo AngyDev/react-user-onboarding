90e4c5587d71fc02a39fc10c79a6ed8b423abc65
Angela Busato <busato.angela@gmail.com>
2021-05-03 14:52:31 +0200

    [add] new main component

---
+++ src/scripts/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <div className="main__container flex flex-col">
+            <h1 style={{ fontFamily: "Montserrat Alternates" }}>Hello World</h1>
+            
+        </div>
+    )
+}
