583688efa7f7b1a262dcc78a105888db1e607f7f
Angela Busato <busato.angela@gmail.com>
2021-05-03 18:21:37 +0200

    [add] checklist component definition

---
+++ src/scripts/components/Checklist.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Checklist() {
+    return (
+        <div>
+            <ul>
+                <li>Contact details</li>
+                <li>Investment plans</li>
+                <li>Investment preferences</li>
+            </ul>
+        </div>
+    )
+}
--- src/scripts/components/Sidebar.js
+++ src/scripts/components/Sidebar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "./Logo";
+import Checklist from "./Checklist";
 
 export default function Sidebar() {
     return (
         <div className="sidebar sidebar__container flex flex-col">
             <Logo />
+            <Checklist />
         </div>
     )
 }
