879b5a61ccf5bc3ba67b30c8e95b3a820e6e48a1
Angela Busato <busato.angela@gmail.com>
2021-05-03 14:52:05 +0200

    [add] Sidebar component

---
+++ src/scripts/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Logo from "./Logo";
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar sidebar__container flex flex-col">
+            <Logo />
+        </div>
+    )
+}
