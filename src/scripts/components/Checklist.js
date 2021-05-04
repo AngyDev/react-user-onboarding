723ca47ad2b68a1b1bc57c2b9063ba954768e6bf
Angela Busato <busato.angela@gmail.com>
2021-05-04 16:04:38 +0200

    [fix] the checklist with a new item component

--- src/scripts/components/Checklist.js
+++ src/scripts/components/Checklist.js
@@ -1,12 +1,12 @@
 import React from "react";
+import ChecklistItem from "./ChecklistItem";
 
 export default function Checklist() {
+    const stepTitles = ["Contact details", "Investment preferences", "Investment plans"];
     return (
         <div>
-            <ul>
-                <li>Contact details</li>
-                <li>Investment plans</li>
-                <li>Investment preferences</li>
+            <ul className="checklist">
+                { stepTitles.map((title, i) => <ChecklistItem key={i} stepTitle={title} lastTitle = {stepTitles.length === i+1}/>) }
             </ul>
         </div>
     )
