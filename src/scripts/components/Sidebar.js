d2b718eb794946b2094113252a9b1ff16c111596
Angela Busato <busato.angela@gmail.com>
2021-05-04 18:22:16 +0200

    [add] quote component

--- src/scripts/components/Sidebar.js
+++ src/scripts/components/Sidebar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Logo from "./Logo";
 import Checklist from "./Checklist";
+import Quote from "./Quote";
 
 export default function Sidebar() {
     return (
-        <div className="sidebar sidebar__container flex flex-col">
+        <div className="sidebar sidebar__container flex flex-col justify-between">
             <Logo />
             <Checklist />
+            <Quote />
         </div>
     )
 }
