e5fafbd64ea06023244afd8757c20d91c39c8891
Angela Busato <busato.angela@gmail.com>
2021-05-05 11:49:23 +0200

    [add] step context and step data from file

--- src/scripts/components/Checklist.js
+++ src/scripts/components/Checklist.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useContext} from "react";
 import ChecklistItem from "./ChecklistItem";
+import steps from "../data/steps.json";
+import {StepContext} from "../App";
 
 export default function Checklist() {
-    const stepTitles = ["Contact details", "Investment preferences", "Investment plans"];
+    const step = useContext(StepContext);
     return (
         <div>
             <ul className="checklist">
-                { stepTitles.map((title, i) => <ChecklistItem key={i} stepTitle={title} lastTitle = {stepTitles.length === i+1}/>) }
+                { steps.stepTitles.map((title, i) => <ChecklistItem key={i} stepTitle={title} lastTitle = {steps.stepTitles.length === i+1} active={i+1 <= step ? true : false}/>) }
             </ul>
             <hr className="spacing spacing--sm"></hr>
         </div>
