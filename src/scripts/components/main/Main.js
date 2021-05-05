4d6fb1e5347d73e89039631caac52657ce4e2b68
Angela Busato <busato.angela@gmail.com>
2021-05-05 17:06:37 +0200

    [add] header component

--- src/scripts/components/main/Main.js
+++ src/scripts/components/main/Main.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "../header/Header";
 
 export default function Main() {
     return (
-        <div className="main__container flex flex-col">
-            <h1 style={{ fontFamily: "Montserrat Alternates" }}>Hello World</h1>
-            
+        <div className="main">
+            <hr className="spacing spacing--2"/>
+            <div className="main__container flex flex-col">
+                <Header />
+            </div>
+            <hr className="spacing spacing--3"/>
         </div>
     )
 }
