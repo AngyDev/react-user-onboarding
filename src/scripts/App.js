7602abe9077a99248011b180948cd8e24c925e9e
Angela Busato <busato.angela@gmail.com>
2021-05-05 11:46:54 +0200

    [add] step context and provider

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
+//import {StepProvider} from "./context/StepContext";
 
-export default class App extends React.Component {
-    render() {
-        return (
+export const StepContext = React.createContext();
+
+export default function App() {
+
+    const [step, setStep] = useState(1);
+
+    return (
+        <StepContext.Provider value={step}>
             <div className="app-container">
                 <Sidebar />
                 <Main />
             </div>
-        )
-    }
+        </StepContext.Provider>
+    )
+
+    /*return (
+        <StepProvider>
+            <div className="app-container">
+                <Sidebar />
+                <Main />
+            </div>
+        </StepProvider>
+    )*/
 }
