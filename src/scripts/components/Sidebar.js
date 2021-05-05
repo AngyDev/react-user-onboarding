4d68bfa5957bfcdfd5df6bc8d94f704e4ccee5a7
Angela Busato <busato.angela@gmail.com>
2021-05-05 11:48:27 +0200

    [add] step context

--- src/scripts/components/Sidebar.js
+++ src/scripts/components/Sidebar.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Logo from "./Logo";
 import Checklist from "./Checklist";
 import Quote from "./Quote";
+import quotes from "../data/quotes.json";
+import {StepContext} from "../App";
 
 export default function Sidebar() {
+    const step = useContext(StepContext);
     return (
         <div className="sidebar sidebar__container flex flex-col justify-between">
             <Logo />
             <Checklist />
-            <Quote />
+            <Quote text={quotes.quotes[step].text} authorName={quotes.quotes[step].authorName} authorRole={quotes.quotes[step].authorRole}/>
         </div>
     )
 }
