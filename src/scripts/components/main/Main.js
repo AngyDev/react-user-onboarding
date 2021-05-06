5221399bb9dfbe4e1f31fa33c4ba17920a616d59
Angela Busato <busato.angela@gmail.com>
2021-05-06 14:54:27 +0200

    [add] the header and the navbar component

--- src/scripts/components/main/Main.js
+++ src/scripts/components/main/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../header/Header";
+import Navbar from "../navbar/Navbar";
 
 export default function Main() {
     return (
-        <div className="main">
+        <div className="main flex flex-col">
             <hr className="spacing spacing--2"/>
-            <div className="main__container flex flex-col">
+            <div className="main__container flex flex-col justify-between">
                 <Header />
+                <Navbar />
             </div>
             <hr className="spacing spacing--3"/>
         </div>
