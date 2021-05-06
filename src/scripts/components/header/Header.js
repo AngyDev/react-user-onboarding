9205528dce3c672ffa0ac136fab81699bde4d12d
Angela Busato <busato.angela@gmail.com>
2021-05-06 14:53:21 +0200

    [add] header button and style

--- src/scripts/components/header/Header.js
+++ src/scripts/components/header/Header.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StepContext } from "../../context/StepContext";
+import Button from "../button/Button";
 
+/**
+ * Header Component
+ * @returns The header render
+ */
 export default function Header() {
-    return(
-        <div>Hello World</div>
+    const [step] = useContext(StepContext);
+    return (
+        <div name="header" className="header flex justify-between align-center">
+            <div>STEP {step} OF 3</div>
+            <div className="flex align-center">
+                <div>Lost or have trouble?</div>
+                <Button typeClass="link" text="Get help" arrow="left"/>
+            </div>
+        </div>
     )
 }
