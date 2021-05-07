002a44b7bd7b59d5d7e56492fa80e9086b2702ce
Angela Busato <busato.angela@gmail.com>
2021-05-07 19:20:55 +0200

    [add] test to render the steps

--- src/scripts/components/step/Step.spec.js
+++ src/scripts/components/step/Step.spec.js
@@ -22,10 +22,28 @@ describe("Step test", () => {
 
     afterEach(cleanup);
 
-    it("should render three button component", () => {
+    it("should render the step", () => {
         renderStep(1);
 
         expect(screen.getByTestId("step")).toBeInTheDocument();
+    });
+
+    it("should render the contact component in the first step", () => {
+        renderStep(1);
+
+        expect(screen.getByTestId("contact")).toBeInTheDocument();
+    });
+
+    it("should render the investment plans component in the second step", () => {
+        renderStep(2);
+
+        expect(screen.getByTestId("investment-plans")).toBeInTheDocument();
+    });
+
+    it("should render the investment preferences component in the third step", () => {
+        renderStep(3);
+
+        expect(screen.getByTestId("investment-preferences")).toBeInTheDocument();
     });
 
 })
