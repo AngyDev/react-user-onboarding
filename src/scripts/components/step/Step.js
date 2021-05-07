42791f8fa21adedbf70d3b215c69e5aec1a9fee4
Angela Busato <busato.angela@gmail.com>
2021-05-07 17:43:30 +0200

    [add] contact component in the step

--- src/scripts/components/step/Step.js
+++ src/scripts/components/step/Step.js
@@ -1,15 +1,19 @@
 import React, { useContext } from "react";
-import steps from "../../data/steps.json";
 import { StepContext } from "../../context/StepContext";
+import steps from "../../data/steps.json";
+import ContactDetails from "../../components/contactDetails/ContactDetails";
 
 export default function Step() {
 
     const [step, setStep] = useContext(StepContext);
 
-    return(
+    return (
         <div data-testid="step" name="step" className="flex flex-col">
-            <div className="step__title">{steps.stepPage[step].title}</div>
-            <div className="step__caption">{steps.stepPage[step].caption}</div>
+            <div className="step">
+                <div className="step__title">{steps.stepPage[step].title}</div>
+                <div className="step__caption">{steps.stepPage[step].caption}</div>
+            </div>
+            <ContactDetails />
         </div>
     )
 }
