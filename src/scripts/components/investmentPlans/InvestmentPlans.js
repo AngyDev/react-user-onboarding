0acf6eca3b0edee2b2188f3493eb2501527c6701
Angela Busato <busato.angela@gmail.com>
2021-05-10 18:18:13 +0200

    [add] Radio button component

--- src/scripts/components/investmentPlans/InvestmentPlans.js
+++ src/scripts/components/investmentPlans/InvestmentPlans.js
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
-import { StepContext } from "../../context/StepContext";
+import RadioBox from "../radioBox/RadioBox";
 import useForm from "../../useForm";
 
 export default function InvestmentPlans() {
 
-    const { handleChange, values, handleSubmit } = useForm();
+    const { handleChange, values, handleSubmit, errors } = useForm();
 
     return (
         <div data-testid="investment-plans" className="flex flex-col">
@@ -15,19 +15,35 @@ export default function InvestmentPlans() {
                 <div className="form__item flex flex-row justify-between">
                     <div className="form__name col-2">
                         <label htmlFor="from" className="form__label flex flex-col">From
-                            <input 
-                                className="form__element form__input" 
-                                type="text" 
-                                name="from" 
+                            <input
+                                className="form__element form__input"
+                                type="text"
+                                name="from"
                                 value={values.from}
                                 onChange={handleChange}
-                                required />
+                            />
                         </label>
+                        {errors.from && <span className="form__error">{errors.from}</span>}
                     </div>
                     <div className="col-2">
                         <label htmlFor="to" className="form__label flex flex-col">To
-                            <input className="form__element form__input" type="text" name="to" maxLength="10" />
+                            <input
+                                className="form__element form__input"
+                                type="text"
+                                name="to"
+                                maxLength="10"
+                                value={values.to}
+                                onChange={handleChange}
+                            />
                         </label>
+                        {errors.to && <span className="form__error">{errors.to}</span>}
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <div className="investment__question">Are you an accredited investor?</div>
+                    <div className="flex flex-row">
+                        <RadioBox labelFor="yes" label="Yes" name="radio" value={true} checked={values.radio === "true"} handleChange={handleChange} />
+                        <RadioBox labelFor="no" label="No" name="radio" value={false} checked={values.radio === "false"} handleChange={handleChange} />
                     </div>
                 </div>
             </form>
