8aa34c1107b45162c6b3cb1c6ceda392c5effd1a
Angela Busato <busato.angela@gmail.com>
2021-05-10 18:17:21 +0200

    [add] the validation controls in a class

---
+++ src/scripts/validationForm.js
@@ -0,0 +1,47 @@
+export default class ValidationForm {
+
+    validateContact(values) {
+        let errors = {};
+
+        if (!values.name.trim()) {
+            errors.name = "Name is required";
+        }
+
+        // Phone
+        if (!values.phone.trim()) {
+            errors.phone = "Phone is required";
+        } else if (!/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*/.test(values.phone)) {
+            errors.phone = "Enter a valid phone";
+        }
+
+        // Email
+        if (!values.email) {
+            errors.email = "Email is required";
+        } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+            errors.email = "Enter a valid email";
+        }
+
+        // Country
+        if (!values.country) {
+            errors.country = "Country is required";
+        }
+
+        return errors;
+    }
+
+    validateInvestmentPlans(values) {
+        let errors = {};
+
+        // From 
+        if (!values.from) {
+            errors.from = "From is required";
+        }
+
+        // To
+        if (!values.to) {
+            errors.to = "To is required";
+        }
+
+        return errors;
+    }
+}
