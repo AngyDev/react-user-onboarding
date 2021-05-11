fe92ac08d5a40c0a165441a58ca5e691c8738ce0
Angela Busato <busato.angela@gmail.com>
2021-05-11 09:07:32 +0200

    [add] validation test for empty inputs

---
+++ src/scripts/validationForm.spec.js
@@ -0,0 +1,60 @@
+import Validation from "./validationForm";
+
+describe("Validate form test", () => {
+
+    const values = {
+        name: "",
+        phone: "",
+        email: "",
+        country: "",
+        from: "",
+        to: "",
+        radio: ""
+    }
+
+    const validation = new Validation();
+
+    it("validate empty input", () => {
+        const errors = validation.validateContact(values);
+
+        expect(errors.name).toBe("Name is required");
+        expect(errors.phone).toBe("Phone is required");
+        expect(errors.email).toBe("Email is required");
+        expect(errors.country).toBe("Country is required");
+    });
+
+    it("validate invalid phone", () => {
+        values.phone = "abcdefg";
+
+        const errors = validation.validateContact(values);
+
+        expect(errors.phone).toBe("Enter a valid phone");
+    });
+
+    it("validate invalid email", () => {
+        values.email = "abcdefg";
+
+        const errors = validation.validateContact(values);
+
+        expect(errors.email).toBe("Enter a valid email");
+    });
+
+    it("validate without errors", () => {
+        values.name = "Test";
+        values.phone = "123456789";
+        values.email = "test@gmail.com";
+        values.country = "Italy";
+
+        const errors = validation.validateContact(values);
+
+        expect(errors).toEqual({});
+    });
+
+    it("validate investment plans empty imputs", () => {
+        const errors = validation.validateInvestmentPlans(values);
+
+        expect(errors.from).toBe("From is required");
+        expect(errors.to).toBe("To is required");
+        expect(errors.radio).toBe("Answer is required");
+    });
+})
