5e1775808cd24fb6fb91c271f5dbf30e78557c87
Angela Busato <busato.angela@gmail.com>
2021-05-11 13:43:59 +0200

    [add] Price input component

--- src/scripts/components/investmentPlans/InvestmentPlans.js
+++ src/scripts/components/investmentPlans/InvestmentPlans.js
@@ -1,11 +1,18 @@
 import React, { useContext, useState } from "react";
 
-import RadioBox from "../radioBox/RadioBox";
+import Radiobox from "../radiobox/Radiobox";
+import PriceInput from "../priceInput/PriceInput";
+import PriceSlider from "../priceSlider/PriceSlider";
 import useForm from "../../useForm";
 
 export default function InvestmentPlans() {
 
     const { handleChange, values, handleSubmit, errors } = useForm();
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
 
     return (
         <div data-testid="investment-plans" className="flex flex-col">
@@ -14,36 +21,22 @@ export default function InvestmentPlans() {
 
                 <div className="form__item flex flex-row justify-between">
                     <div className="form__name col-2">
-                        <label htmlFor="from" className="form__label flex flex-col">From
-                            <input
-                                className="form__element form__input"
-                                type="text"
-                                name="from"
-                                value={values.from}
-                                onChange={handleChange}
-                            />
-                        </label>
+                        <PriceInput name="from" min="10000" max="500000" label="From" value={values.from} onChange={handleChange} />
                         {errors.from && <span className="form__error">{errors.from}</span>}
                     </div>
                     <div className="col-2">
-                        <label htmlFor="to" className="form__label flex flex-col">To
-                            <input
-                                className="form__element form__input"
-                                type="text"
-                                name="to"
-                                maxLength="10"
-                                value={values.to}
-                                onChange={handleChange}
-                            />
-                        </label>
+                        <PriceInput name="to" min="50000" label="To" value={values.to} onChange={handleChange} />
                         {errors.to && <span className="form__error">{errors.to}</span>}
                     </div>
+                </div>
+                <div className="form__item">
+                    <PriceSlider />
                 </div>
                 <div className="flex flex-col">
                     <div className="investment__question">Are you an accredited investor?</div>
                     <div className="flex flex-row">
-                        <RadioBox labelFor="yes" label="Yes" name="radio" value={true} checked={values.radio === "true"} handleChange={handleChange} />
-                        <RadioBox labelFor="no" label="No" name="radio" value={false} checked={values.radio === "false"} handleChange={handleChange} />
+                        <Radiobox labelFor="yes" label="Yes" name="radio" value={true} checked={values.radio === "true"} handleChange={handleChange} />
+                        <Radiobox labelFor="no" label="No" name="radio" value={false} checked={values.radio === "false"} handleChange={handleChange} />
                     </div>
                     {errors.radio && <span className="form__error">{errors.radio}</span>}
                 </div>
