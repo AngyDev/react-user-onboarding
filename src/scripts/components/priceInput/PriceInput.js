ce2f05c7d903b5b3b076de90f524ba6c14af9eb7
Angela Busato <busato.angela@gmail.com>
2021-05-11 13:42:16 +0200

    [add] price input component

---
+++ src/scripts/components/priceInput/PriceInput.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+
+export default function PriceInput(props) {
+
+    const [isEditing, setIsEditing] = useState(false)
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
+    const toggleEditing = () => {
+        setIsEditing(!isEditing);
+    }
+
+    return (
+        <label htmlFor={props.name} className="form__label flex flex-col">{props.label}
+        {isEditing ? (
+            <input
+                className="form__element form__input"
+                type="number"
+                name={props.name}
+                min={props.min}
+                max={props.max}
+                onBlur={toggleEditing}
+                value={props.value}
+                placeholder="$0,00"
+                onChange={props.onChange}
+            />
+        ) : (
+            <input
+                className="form__element form__input"
+                type="text"
+                value={formatter.format(props.value)}
+                placeholder="$0,00"
+                onFocus={toggleEditing}
+                readOnly
+            />
+        )}
+        </label>
+    )
+}
