09ba13e374b1cc31cb332243797a89f1a770bcbd
Angela Busato <busato.angela@gmail.com>
2021-05-12 12:32:08 +0200

    [add] error if checkbox is not checked

--- src/scripts/components/investmentPreferences/InvestmentPreferences.js
+++ src/scripts/components/investmentPreferences/InvestmentPreferences.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import Checkbox from "../checkbox/Checkbox";
 import preferences from "../../data/preferences.json";
+import Validation from "../../validationForm";
 
 export default function InvestmentPlans() {
 
+    const validation = new Validation();
+
     let array = [];
+    const [errors, setErrors] = useState({});
     
     const handleChange = e => {
         const target = e.target;
@@ -25,6 +29,9 @@ export default function InvestmentPlans() {
 
     function handleSubmit(event) {
         event.preventDefault();
+
+        setErrors(validation.validateInvestmentPreferences(array));
+
         console.log("Submit of the form");
     }
 
@@ -49,6 +56,7 @@ export default function InvestmentPlans() {
                         })
                     }
                 </div>
+                {errors.check && <span className="form__error">{errors.check}</span>}
             </form>
         </div>
     )
