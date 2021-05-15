88c917d2ef367d231940eee488bd82715e48290a
Angela Busato <busato.angela@gmail.com>
2021-05-15 11:00:52 +0200

    [add] start and end values to change the active bar

--- src/scripts/components/priceSlider/PriceSlider.js
+++ src/scripts/components/priceSlider/PriceSlider.js
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function PriceSlider(props) {
 
-    const [prices] = useState([10000,50000,100000,200000,500000,1000000]);
+    const [prices] = useState([10000, 50000, 100000, 200000, 500000, 1000000]);
+    const [startBar, setStartBar] = useState(0);
+    const [endBar, setEndBar] = useState(1);
+
+    useEffect(() => {
+        prices.forEach((item, i) => {
+            if (props.start >= item) { setStartBar(i) }
+        })
+    });
+
+    useEffect(() => {
+        prices.forEach((item, i) => {
+            if (props.end >= item) { setEndBar(i) }
+        })
+    });
 
     const formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
@@ -11,23 +25,17 @@ export default function PriceSlider(props) {
 
     return (
         <div>
-            <div className="slider slider--background">
-                <div className="slider slider--active" style={{ left: 0, width: "75%" }}>
-                    <span style={{ position: "absolute", left: "0%" }}>
-                        <div className="slider__pointer"></div>
-                    </span>
-                    <span style={{ position: "absolute", left: "75%" }}>
-                        <div className="slider__pointer"></div>
-                    </span>
+            <div className="slider">
+                <div className="slider--background">
+                    {prices.map((item, i) => ((i < (prices.length - 1)) && <span key={i} className="slider__line"></span>))}
+                    {/* OK prices.map((item, i) => ((i < (prices.length - 1)) && ((i >= startBar && i < endBar) ? <span key={i} className="slider__line slider--active"></span> : <span key={i} className="slider__line"></span>)))*/}
+                </div>
+                <div className="slider__active-bar" style={{ left: startBar > 0 ? `${startBar*20}%`: '0'}}>
+                    {prices.map((item, i) => ((i < (prices.length - 1)) && ((i >= startBar && i < endBar) && <span key={i} className="slider__line--active"></span>)))}
                 </div>
             </div>
-            <div className="slider__label flex flex-row">
-            {
-                prices.map((el,i) => (
-                    <span key={i} >{formatter.format(el)}{i === prices.length-1 ? '+' : ''}</span>
-                )
-                )
-            }
+            <div className="slider__label">
+                {prices.map((item, i) => (<span key={i} >{formatter.format(item).replace(/\D00$/, '')}{i === prices.length - 1 && "+"}</span>))}
             </div>
         </div>
     )
