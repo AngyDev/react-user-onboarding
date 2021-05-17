7e60de7c22ba1b61570c84331706f8e9dbad24e7
Angela Busato <busato.angela@gmail.com>
2021-05-17 11:35:55 +0200

    [add] function to check errors and to go to next step

--- src/scripts/useForm.js
+++ src/scripts/useForm.js
@@ -7,51 +7,47 @@ const useForm = () => {
     const [step, setStep] = useContext(StepContext);
     const [values, setValues] = useContext(UserContext);
 
-    /*const [values, setValues] = useState({
-        name: "",
-        phone: "",
-        email: "",
-        country: "",
-        from: 10000,
-        to: 200000,
-        radio: ""
-    });*/
-
     const [errors, setErrors] = useState({});
 
     const validation = new Validation();
 
     const handleChange = (e) => {
+
         const {name, value} = e.target;
+
         setValues({
             ...values,
             [name]: value
         });
+
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if (step === 1) {
-            const validateContact = validation.validateContact(values);
-            setErrors(validateContact);
+            const contactErrors = validation.validateContact(values);
+            setErrors(contactErrors);
             
-            // Checks if there aren't errors and goes to the next step
-            if(Object.keys(validateContact).length === 0) {
-                setStep(step + 1);
-            }
-        } else if (step == 2) {
-            const validatePlans = validation.validateInvestmentPlans(values);
-            setErrors(validatePlans);
+            checksErrors(contactErrors);
+
+        } else if (step === 2) {
+            const plansErrors = validation.validateInvestmentPlans(values);
+            setErrors(plansErrors);
             
-            // Checks if there aren't errors and goes to the next step
-            if(Object.keys(validatePlans).length === 0) {
-                setStep(step + 1);
-            }
+            checksErrors(plansErrors);
         }
-        
-        console.log("name " + values.name + " from " + values.from);
-        console.log(values);
+
+    }
+
+    /**
+     * Checks if there aren?t errors and goes to the next step
+     * @param {Object} errors 
+     */
+    const checksErrors = (errors) => {
+        if(Object.keys(errors).length === 0) {
+            setStep(step + 1);
+        }
     }
 
     return { handleChange, values, handleSubmit, errors };
