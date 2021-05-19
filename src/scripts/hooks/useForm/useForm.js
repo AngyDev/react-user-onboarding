44daa4447976042f27120a615f869855eaf3dd0f
Angela Busato <busato.angela@gmail.com>
2021-05-19 18:14:59 +0200

    [refactor] useForm handleSubmit and handleChange

--- src/scripts/hooks/useForm/useForm.js
+++ src/scripts/hooks/useForm/useForm.js
@@ -11,9 +11,27 @@ const useForm = () => {
 
     const validation = new Validation();
 
-    const handleChange = (e) => {
+    /**
+     * A global handle change
+     * @param {Event} e - The event of the page
+     * @param {String} type - The type of value that the function has to add
+     * @returns 
+     */
+    const handleChange = (e, type) => {
 
-        const {name, value} = e.target;
+        const { name, value } = e.target;
+
+        if (type === "array") {
+            if (!values.preferences.includes(value)) {
+                return setValues(prevUser => {
+                    return { ...prevUser, [name]: [...prevUser.[name], value] }
+                });
+            } else {
+                return setValues(prevUser => {
+                    return { ...prevUser, [name]: [...prevUser.[name], value].filter(el => el !== value) }
+                });
+            }
+        }
 
         setValues({
             ...values,
@@ -22,31 +40,19 @@ const useForm = () => {
 
     }
 
-    const handleSubmit = (e) => {
+    /**
+     * A generic handle submit that checks if there is validation errors and then return the callback
+     * @param {Event} e - The event page
+     * @param {Object} validateForm - The object of the response of validation
+     * @param {Function} callback - A function of callback 
+     */
+    const handleSubmit = (e, validateForm, callback) => {
         e.preventDefault();
 
-        if (step === 1) {
-            const contactErrors = validation.validateContact(values);
-            setErrors(contactErrors);
-            
-            checksErrors(contactErrors);
-
-        } else if (step === 2) {
-            const plansErrors = validation.validateInvestmentPlans(values);
-            setErrors(plansErrors);
-            
-            checksErrors(plansErrors);
-        }
-
-    }
-
-    /**
-     * Checks if there aren?t errors and goes to the next step
-     * @param {Object} errors 
-     */
-    const checksErrors = (errors) => {
-        if(Object.keys(errors).length === 0) {
-            setStep(step + 1);
+        if (Object.keys(validateForm).length !== 0) {
+            setErrors(validateForm);
+        } else {
+            callback();
         }
     }
 
