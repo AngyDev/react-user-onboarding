b9d55918437fbb0bb0a68590b9972e8beec396c8
Angela Busato <busato.angela@gmail.com>
2021-05-19 13:07:48 +0200

    [add] new folder features to page App

---
+++ src/scripts/features/App/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Main from "../../components/main/Main";
+import { StepContext } from "../../context/StepContext";
+import { UserProvider } from "../../context/UserContext";
+
+export default function App() {
+
+    const [step, setStep] = useState(1);
+
+    return (
+        <StepContext.Provider value={[step, setStep]}>
+            <UserProvider >
+                <div className="app-container">
+                    <Sidebar />
+                    <Main />
+                </div>
+            </UserProvider>
+        </StepContext.Provider>
+    )
+
+    /*return (
+        <StepProvider>
+            <div className="app-container">
+                <Sidebar />
+                <Main />
+            </div>
+        </StepProvider>
+    )*/
+}
