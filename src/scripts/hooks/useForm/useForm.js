30c973392b83303dd9b095cb49603c7abad6ac81
Angela Busato <busato.angela@gmail.com>
2021-05-19 13:08:12 +0200

    [add] folder for custom hooks

---
+++ src/scripts/hooks/useForm/useForm.js
@@ -0,0 +1,56 @@
+import { useState, useContext } from 'react';
+import { StepContext } from "../../context/StepContext";
+import Validation from "../../validation/validationForm";
+import { UserContext } from "../../context/UserContext";
+
+const useForm = () => {
+    const [step, setStep] = useContext(StepContext);
+    const [values, setValues] = useContext(UserContext);
+
+    const [errors, setErrors] = useState({});
+
+    const validation = new Validation();
+
+    const handleChange = (e) => {
+
+        const {name, value} = e.target;
+
+        setValues({
+            ...values,
+            [name]: value
+        });
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (step === 1) {
+            const contactErrors = validation.validateContact(values);
+            setErrors(contactErrors);
+            
+            checksErrors(contactErrors);
+
+        } else if (step === 2) {
+            const plansErrors = validation.validateInvestmentPlans(values);
+            setErrors(plansErrors);
+            
+            checksErrors(plansErrors);
+        }
+
+    }
+
+    /**
+     * Checks if there aren?t errors and goes to the next step
+     * @param {Object} errors 
+     */
+    const checksErrors = (errors) => {
+        if(Object.keys(errors).length === 0) {
+            setStep(step + 1);
+        }
+    }
+
+    return { handleChange, values, handleSubmit, errors };
+}
+
+export default useForm;
---
+++ src/scripts/hooks/useForm/useForm.spec.js
@@ -0,0 +1,14 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import useForm from "./useForm";
+
+describe("use form test", () => {
+
+    afterEach(cleanup);
+
+    //const { handleChange, values, handleSubmit, errors } = useForm();
+
+    it("", () => {
+
+    });
+
+})
