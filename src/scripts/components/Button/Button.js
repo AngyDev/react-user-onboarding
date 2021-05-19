49d92652f6031691924f98064df0866736f83482
Angela Busato <busato.angela@gmail.com>
2021-05-19 14:05:20 +0200

    [add] Button forlder

---
+++ src/scripts/components/Button/Button.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+/**
+ * The button Component
+ * @param {Props} props - The class type of the button, the text of the button, the arrow and onclick function
+ * @returns The button render
+ */
+export default function Button(props) {
+
+    let leftArrow = "";
+    let rightArrow = "";
+
+    if(props.arrow === "left") {
+        leftArrow = <i className="icon-arrow-left"></i>;
+    } else if (props.arrow === "right") {
+        rightArrow = <i className="icon-arrow-right"></i>;
+    }
+
+    return(
+        <button data-testid="button" className={`button ${props.typeClass} flex align-center`} form={props.form} onClick={props.handleClick}>{rightArrow}{props.text}{leftArrow}</button>
+    )
+}
