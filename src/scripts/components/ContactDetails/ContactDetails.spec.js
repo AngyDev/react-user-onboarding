b461978dace385322339a2b5ac611b842cb31b6b
Angela Busato <busato.angela@gmail.com>
2021-05-27 12:20:38 +0200

    [add] test of onChange on input field

--- src/scripts/components/ContactDetails/ContactDetails.spec.js
+++ src/scripts/components/ContactDetails/ContactDetails.spec.js
@@ -2,12 +2,8 @@ import React from "react";
 import { cleanup, render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import Input from "../Input/Input";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 import ContactDetails from "./ContactDetails";
 
@@ -55,35 +51,12 @@ describe("Contact details test", () => {
         renderContactDetails();
         const input = screen.getByLabelText(/name/i);
         const inputValue = "Angela";
-        const mockChange = jest.fn();
 
         expect(input.value).toEqual("");
-        input.onChange = mockChange;
+
         fireEvent.change(input, { target: { value: inputValue } });
+        
         expect(input.value).toBe("Angela");
-        //expect(mockChange).toHaveBeenCalled();
     });
-
-    /*it("onChange() is called upon changing the text field", () => {
-        /*const value = "Makers Academy";
-        const onChange = jest.fn();
-        const wrapper = shallow(<ContactDetails onChange={onChange} />);
-        const result = wrapper.find('#form-id');
-        expect(result.length).toEqual(1);
-        expect(wrapper.find(Input)).toHaveLength(3);
-        const input = wrapper.find(Input);
-        input.simulate('change', { target: { value } });
-        expect(onChange.called).toBe(true);
-        /*result.simulate('change', {target: {value}});
-        expect(onChange.called).toBe(true);*/
-
-        /*const wrapper = shallow(<ContactDetails />);
-        const instance = wrapper.instance();
-
-        jest.spyOn(instance, 'onChange');
-
-        wrapper.find('select').simulate('change');
-        expect(instance.onSubmit).toHaveBeenCalled();
-    });*/
 
 });
