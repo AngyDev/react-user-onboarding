f246223bc63a1d445bfd0e0364a26f0e1e2cce08
Angela Busato <busato.angela@gmail.com>
2021-05-28 17:20:31 +0200

    [add] new file for preferences checkbox array

---
+++ src/scripts/data/preferences.js
@@ -0,0 +1,34 @@
+export const preferences = [
+    {
+        value: "Single Family",
+        isChecked: false
+    },
+    {
+        value: "Residential multifamily",
+        isChecked: false
+    },
+    {
+        value: "Commercial retail",
+        isChecked: false
+    },
+    {
+        value: "Commercial industrial",
+        isChecked: false
+    },
+    {
+        value: "Commercial hospitality",
+        isChecked: false
+    },
+    {
+        value: "Commercial warehousing",
+        isChecked: false
+    },
+    {
+        value: "Commercial office",
+        isChecked: false
+    },
+    {
+        value: "Other",
+        isChecked: false
+    }
+];
