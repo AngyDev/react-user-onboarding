becbab1e5e5d8ee71b0f9a3f21fdf3b001051011
Angela Busato <busato.angela@gmail.com>
2021-05-28 17:26:38 +0200

    [add] useForm commented test

--- src/scripts/hooks/useForm/useForm.spec.js
+++ src/scripts/hooks/useForm/useForm.spec.js
@@ -1,5 +1,9 @@
+import React from "react";
 import { cleanup, render, screen } from "@testing-library/react";
+import { renderHook, act } from '@testing-library/react-hooks';
 import useForm from "./useForm";
+
+import { UserContext, UserProvider } from "../../context/UserContext";
 
 describe("use form test", () => {
 
@@ -7,8 +11,30 @@ describe("use form test", () => {
 
     //const { handleChange, values, handleSubmit, errors } = useForm();
 
-    it("", () => {
+    it("should handle the changes of the user object", () => {
+        //const wrapper = ({ children }) => <UserProvider>{children}</UserProvider>;
+        /*const user = {
+            name: "",
+            phone: "",
+            email: "",
+            country: "",
+            from: 10000,
+            to: 200000,
+            radio: "",
+            preferences: []
+        };
+        const wrapper = ({ wrapper }) => <UserProvider values={[user]}>{children}</UserProvider>;
 
+        const { result } = renderHook(() => useForm(), { wrapper });
+        console.log(useForm().handleChange({}));*/
+        /*const e = {
+            target: {
+                name: "name",
+                value: "Angela"
+            }
+        }
+ 
+        act(() => result.current.handleChange(e));
+       console.log(); */
     });
-
 })
