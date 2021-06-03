3a0f6272fc473bc0e7706d3f1edc9657ab1dd5eb
Angela Busato <busato.angela@gmail.com>
2021-06-03 13:27:34 +0200

    [add] privacy policy modal content

---
+++ src/scripts/components/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PrivacyPolicy() {
+    return (
+        <div className="font__mont-base">
+            <p>A Privacy Policy is a legal statement that specifies what the business owner does with the personal data collected from users, along with how the data is processed and for what purposes.</p>
+
+            <p>In 1968, Council of Europe did studies on the threat of the Internet expansion as they were concerned with the effects of technology on human rights. This lead to the development of policies that were to be developed to protect personal data.</p>
+        </div>
+    )
+}
