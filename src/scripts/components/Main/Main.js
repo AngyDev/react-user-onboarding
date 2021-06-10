6bb9af744aa1948574d0eeb8a4151c29367b3627
Angela Busato <busato.angela@gmail.com>
2021-06-10 19:12:35 +0200

    [add] children component to the main component

--- src/scripts/components/Main/Main.js
+++ src/scripts/components/Main/Main.js
@@ -3,16 +3,16 @@ import Header from "../Header/Header";
 import Step from "../Step/Step";
 import Navbar from "../Navbar/Navbar";
 
-export default function Main() {
+export default function Main({ children }) {
     return (
         <div data-testid="main" className="main flex flex-col">
-            <hr className="spacing spacing--2"/>
+            <hr className="spacing spacing--2" />
             <div className="main__container flex flex-col justify-between">
                 <Header />
-                <Step />
+                <Step >{children}</Step>
                 <Navbar />
             </div>
-            <hr className="spacing spacing--3"/>
+            <hr className="spacing spacing--3" />
         </div>
     )
 }
