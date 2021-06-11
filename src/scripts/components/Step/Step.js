1de1365acbc5454cf67ed99b858cc6518b74aea6
Angela Busato <busato.angela@gmail.com>
2021-06-11 10:24:14 +0200

    [remove] unused import

--- src/scripts/components/Step/Step.js
+++ src/scripts/components/Step/Step.js
@@ -1,10 +1,6 @@
 import React, { useContext } from "react";
-import { Route, Switch } from "react-router";
 import { StepContext } from "../../context/StepContext";
 import steps from "../../data/steps.json";
-import ContactDetails from "../ContactDetails/ContactDetails";
-import InvestmentPlans from "../InvestmentPlans/InvestmentPlans";
-import InvestmentPreferences from "../InvestmentPreferences/InvestmentPreferences";
 
 export default function Step({children}) {
 
