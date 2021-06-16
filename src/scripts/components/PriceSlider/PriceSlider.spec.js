d6b7b5afd7c3b73ef2a3d51e4b93760b952cbe85
Angela Busato <busato.angela@gmail.com>
2021-06-16 13:05:50 +0200

    [add] translation context in test files

--- src/scripts/components/PriceSlider/PriceSlider.spec.js
+++ src/scripts/components/PriceSlider/PriceSlider.spec.js
@@ -2,12 +2,18 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import PriceSlider from "./PriceSlider";
 import { act } from "react-dom/test-utils";
+import { TranslationContext } from "../../context/TranslationContext";
+import translations from "../../translations/translations.json";
 
 describe("Price Slider test component", () => {
 
     it("should render the list of prices", () => {
         act(() => {
-            render(<PriceSlider start="10000" end="100000" />);
+            render(
+                <TranslationContext.Provider value={[translations.english]}>
+                    <PriceSlider start="10000" end="100000" />
+                </TranslationContext.Provider>
+            );
         });
 
         expect(screen.getByTestId("price-slider").textContent).toBe("$10,000$50,000$100,000$200,000$500,000$1,000,000+");
