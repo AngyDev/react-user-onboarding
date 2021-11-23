d8b23e6886e0e9da16c6403515f28f9bac66a806
Angela Busato <busato.angela@gmail.com>
2021-11-23 15:22:56 +0100

    add:server and postgres db

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+const knexConfig = require ('./utils/knexfile');
+const { Model } = require ('objection'); 
+const app = express();
+
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const environment = knex(process.env.NODE_ENV === "production" ? knexConfig.production : knexConfig.development);
+Model.knex(environment);
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+app.use('/', require('./routes/user'));
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+})
